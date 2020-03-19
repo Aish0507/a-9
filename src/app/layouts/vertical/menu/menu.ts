c37cded6c8b8a5111eb1a0f6ec4d8d11bdc7b9f0
aish-neo <aishvarya0507@hotmail.com>
2020-03-19 21:00:33 +0530

    Added has in route and login page route

--- src/app/layouts/vertical/menu/menu.ts
+++ src/app/layouts/vertical/menu/menu.ts
@@ -2,7 +2,10 @@ import { Menu } from '../../../core/models/menu.model';
 
 
 export const verticalMenuItems = [
-    new Menu(1, 'Dashboard', '/', null, 'dashboard', null, false, 0),
-    new Menu(2, 'Task', '/blank', null, 'check_box_outline_blank', null, true, 0),
-    new Menu(3, 'Sub Task', '/blank', null, 'check_box_outline_blank', null, false, 2)
+    new Menu(1, 'Dashboard', '/', null, 'bx-home-circle', null, false, 0, true),
+    new Menu(2, 'Task', '/blank', null, 'bx-task', null, true, 0, false),
+    new Menu(3, 'Sub Task', '/sub-task', null, 'bx-subdirectory-right', null, false, 2, false),
+    new Menu(4, 'Sub Task', '/sub-task', null, 'bx-subdirectory-right', null, false, 2, false),
+    new Menu(5, 'Event', '/event', null, 'bx-polygon', null, false, 0, false),
+    new Menu(6, 'Login Demo', '/account/auth/login', null, 'bx-log-in', null, false, 0, false)
 ];
